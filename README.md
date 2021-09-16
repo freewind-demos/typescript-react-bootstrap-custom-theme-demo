686fc1869f9d581e8d1ec6f95ed130853bbcd256
Freewind <nowindlee@gmail.com>
2021-09-16 21:44:09 +0800

    typescript-react-bootstrap-custom-theme-demo

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 TypeScript "react-bootstrap" Custom Theme Demo
 =================================
 
-看起来推荐直接写css来覆盖：
+可以通常给出新的全局css变量来改变全局样式
+- Bootstrap css variables: https://github.com/twbs/bootstrap/blob/main/scss/_variables.scss
+
+推荐直接写css来覆盖：
 https://react-bootstrap.github.io/getting-started/theming/
 
-为了避免冲突，可以使用`ThemeProvider`或者`bsPrefix`属性来自定义前缀，通常不需要这么做
+为了避免冲突，可以使用`ThemeProvider`或者`bsPrefix`属性来自定义前缀，或者通过`varient`属性，给一个不同的后缀。
+通常不需要这么做，因为会丢失bootstrap自己的样式（除非自己重新配置并编译css)
+
+查看：`src/theme.css`
+
+- react-bootstrap: https://react-bootstrap.netlify.app/getting-started
+- components: https://react-bootstrap.netlify.app/components
+- Theming: https://react-bootstrap.github.io/getting-started/theming
+- Bootstrap 5 cheatsheet: https://bootstrap-cheatsheet.themeselection.com/
+
 
 ```
 npm install
--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,21 +1,25 @@
 import React, {FC} from 'react';
-import {Buttons} from './buttons';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './theme.css';
+import {ThemeProvider} from 'react-bootstrap';
+import {Buttons} from './Buttons';
 import {Forms} from './Forms';
 import {Grids} from './Grids';
 import {UtilClassnames} from './UtilClassnames';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './theme.css';
-
 export const Hello: FC = ({}) => {
-  return <div style={{padding: 20}}>
-    <Buttons/>
-    <hr/>
-    <Forms/>
-    <hr/>
-    <Grids/>
-    <hr/>
-    <UtilClassnames/>
-  </div>;
+  return <ThemeProvider prefixes={{btn: 'btn'}}>
+    {
+      <div style={{padding: 20}}>
+        <Buttons/>
+        <hr/>
+        <Forms/>
+        <hr/>
+        <Grids/>
+        <hr/>
+        <UtilClassnames/>
+      </div> as any // FIXME not sure why need this otherwise there is a compilation error
+    }
+  </ThemeProvider>;
 }
